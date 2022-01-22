952e74cf9d47b9bb46f97a7cce16a6421b7f3c6b
Roy Chen <roycyt@gmail.com>
2022-01-22 15:44:23 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "Nikola Tesla"  # (translatable)
 BLOG_TITLE = "My Nikola Site"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://example.com/"
+SITE_URL = "https://roycyt.github.io/nikola_demo/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
